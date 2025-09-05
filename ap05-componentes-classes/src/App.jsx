313037aeedc52851e7ad304555a95b9c37c486d3
Rodrigo Bossini <professorbossini@gmail.com>
2025-09-05 17:56:43 -0300

    learn(ap05-react): encerra a apostila

--- ap05-componentes-classes/src/App.jsx
+++ ap05-componentes-classes/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Lontra from './Lontra'
 class App extends React.Component{
   
   constructor(props){
@@ -8,7 +9,8 @@ class App extends React.Component{
       longitude: null,
       estacao: null,
       data: null,
-      icone: null
+      icone: null,
+      mensagemDeErro: null
     }
   }
 
@@ -51,30 +53,93 @@ class App extends React.Component{
         const icone = this.icones[estacao]
         //this.state.icone = icone
         this.setState({
-          icone: icone
+          icone: icone,
+          estacao: estacao,
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          data: dataAtual
+
         })
       },
       (err) => {
-        console.log(`Erro: ${err}`)        
+        console.log(`Erro: ${err}`)
+        this.setState({
+          mensagemDeErro: 'Tente novamente mais tarde'
+        })
       }
     )
   }
 
-  componentDidMount(){
-    this.obterLocalizacao()
-  }
+  // componentDidMount(){
+  //   this.obterLocalizacao()
+  // }
 
+// agora, a lontra tem um parceiro
+//mais ainda, cada lontra é produzida por um componente React chamado Lontra
   render(){
-    return(
-      <div>
-        <div><i className="fa-otter fa-solid"></i></div>
-        <div><i className={`fa-solid fa-${this.state.icone}`}></i></div>
-        <div>
-          <p>Meu app</p>
+        return(
+        <div className='container mt-2'>
+          <div className='row'>
+            <div className='col-12'>
+              <Lontra tamanho="fa-3x"/>
+              <Lontra tamanho="fa-3x"/>
+            </div>
+          </div>
+          <div className="row">
+              <div className='col-sm-12'>
+                <div className="card">
+                  <div className="card-body">
+                    <div 
+                      className="d-flex align-items-center border rounded mb-2"
+                      style={{height: '6rem'}}>
+                        <i className={`fa-solid fa-4x fa-${this.state.icone}`}></i>
+                        <p className="ms-2 w-75 text-center fs-1">{this.state.estacao}</p>
+                    </div>
+                    <div>
+                      <p className="text-center">
+                        {/* renderização condicional */}
+                        {
+                          this.state.latitude ?
+                          `Coordenadas: ${this.state.latitude},${this.state.longitude}. Data: ${this.state.data.toLocaleString()}`:
+                          this.state.mensagemDeErro ? 
+                          `${this.state.mensagemDeErro}`: 
+                          `Precisa liberar o acesso à localização`
+                        }
+                      </p>
+                    </div>
+                    <button 
+                      className='btn btn-outline-primary w-100 mt-2'
+                      onClick={this.obterLocalizacao}>
+                      Qual a minha estação
+                    </button>
+                  </div>
+                </div>
+              </div>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
-}
 
 export default App
+{/* <p>E eu?</p>
+  <div style={{fontSize: 32}}>
+  <div style={{fontSize: 8}}>
+  Estou usando o que eu defino
+  </div>
+  <div>
+  Estou usando o que eu herdei
+  </div>
+</div> */}
+  // return <div style={{fontSize: '4em'}}>
+  //   Tenho 4em
+  //   <div>
+  //     Herdei 4em
+  //   </div>
+  //   <div style={{fontSize: '2em'}}>
+  //     Herdo 4em, porém defino 2em
+  //   </div>
+  //   <div style={{fontSize: '2rem'}}>
+  //     Eu defino 2rem
+  //   </div>
+  // </div>
---
+++ ap05-componentes-classes/src/Lontra.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Lontra = (props) => {
+  return (
+    <i className={`fa-otter fa-solid ${props.tamanho}`}></i>
+  )
+}
+
+export default Lontra

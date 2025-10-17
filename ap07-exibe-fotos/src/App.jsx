52180a8b4786eb02253cae1bd63515009a59e061
Rodrigo Bossini <professorbossini@gmail.com>
2025-10-17 18:05:26 -0300

    learn(ap07-react): encerra a secao 2.12
    - requisicoes com sdk ok
    - retomar fazendo requisicoes com a axios
    - terminar fazendo responsividade
    - a seguir, comecar com react native

--- ap07-exibe-fotos/src/App.jsx
+++ ap07-exibe-fotos/src/App.jsx
@@ -1,20 +1,58 @@
+import React from "react"
 import Busca from "./components/Busca"
+import { createClient } from 'pexels'
+import PexelsLogo from "./components/PexelsLogo"
 
-const App = () => {
-  return (
-    <div className='grid w-9 m-auto p-2 border-1 border-400 border-round'>
-      <div>
-        <i 
-          className="pi pi-android"
-          style={{fontSize: '2rem'}}></i>
+export default class App extends React.Component {
+
+  //declarar uma variável de estado e guardar o vetor photos nela. ela pode se chamar photos
+  state = {
+    photos: []
+  }
+  pexelsClient = null
+
+
+  onBuscaRealizada = (termo) => {
+    this.pexelsClient.photos.search({
+      query: termo
+    })
+    .then(result => this.setState({photos: result.photos}))
+  }
+
+  componentDidMount(){
+    this.pexelsClient = createClient('a91Qyfh2Ud1rdeOGKV8aTR5Aj9UmRvdma6EdyhC9EfKStoAyt7rmDuhV')
+  }
+
+  render() {
+    return (
+      <div className='grid w-9 m-auto p-2 border-1 border-400 border-round-md'>
+        <div className="col-12">
+          <div className="flex">
+            <PexelsLogo />
+            <div className="ml-3">
+              <i 
+                className="pi pi-android"
+                style={{fontSize: '2rem'}}></i>
+            </div>
+          </div>
+        </div>
+        <div className="col-12">
+          Exibir uma lista de...
+        </div>
+        <div className="col-12">
+          <Busca 
+            onBuscaRealizada={this.onBuscaRealizada}/>
+        </div>
+        <div className="col-12">
+          {
+            this.state.photos.map((photo) => (
+              <div key={photo.id}>
+                <img src={photo.src.small} alt={photo.alt}/>
+              </div>
+            ))
+          }    
+        </div>  
       </div>
-      <div className="col-12">
-        Exibir uma lista de...
-      </div>
-      <div className="col-12">
-        <Busca />
-      </div>  
-    </div>
-  )
+    )
+  }
 }
-export default App
--- ap07-exibe-fotos/src/components/Busca.jsx
+++ ap07-exibe-fotos/src/components/Busca.jsx
@@ -12,23 +12,29 @@ export default class Busca extends React.Component{
     // console.log(evento.target.value)
     this.setState({termoDeBusca: evento.target.value})
   }
+  onFormSubmit = (evento) => {
+    evento.preventDefault()
+    this.props.onBuscaRealizada(this.state.termoDeBusca)
+  }
   render(){
     return (
-      <div className='flex flex-column'>
-        <IconField 
-          iconPosition='left'
-          className='w-full'>
-          <InputIcon className='pi pi-search'/>
-          <InputText
-            value={this.state.termoDeBusca} 
-            className='w-full'
-            onChange={this.onTermoAlterado}
-            placeholder={this.props.dica}/>  
-        </IconField>
-        <Button
-          className='mt-2' 
-          label='OK' />
-      </div>
+      <form onSubmit={this.onFormSubmit}>
+        <div className='flex flex-column'>
+          <IconField 
+            iconPosition='left'
+            className='w-full'>
+            <InputIcon className='pi pi-search'/>
+            <InputText
+              value={this.state.termoDeBusca} 
+              className='w-full'
+              onChange={this.onTermoAlterado}
+              placeholder={this.props.dica}/>  
+          </IconField>
+          <Button
+            className='mt-2' 
+            label='OK' />
+        </div>
+      </form>
     )
   }
 }

dff979109ee8ee07b7e93be000ab1cfad17d6b67
Rodrigo Bossini <professorbossini@gmail.com>
2025-11-07 18:03:25 -0300

    learn(ap002-rn): lida com a pg 40
    - fizemos a remocao
    - fazer a atualizacao, terminando a apostila
    - a seguir, apresentar o projeto para os alunos
    - os alunos terao 2 semanas para fazer, ou seja, esperamos resultados notaveis

--- ap002-rn-crud-lembretes/App.tsx
+++ ap002-rn-crud-lembretes/App.tsx
@@ -1,5 +1,8 @@
 import { 
+  Alert,
   Button,
+  FlatList,
+  Linking,
   Pressable,
   StyleSheet, 
   Text, 
@@ -7,6 +10,7 @@ import {
   View 
 } from 'react-native';
 import { useState } from 'react'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
 
 interface Lembrete{
@@ -26,6 +30,33 @@ export default function App() {
     setLembretes(lembretesAtual => [novoLembrete, ...lembretesAtual])
     setLembrete('')
   }
+
+  const remover = (lembrete: Lembrete) => {
+    // Alert.alert(
+    //   // título
+    //   'Remover lembrete',
+    //   // texto central
+    //   `Deseja remover esse lembrete? ${lembrete.texto}`,
+    //   // lista de botões
+    //   [
+    //     {
+    //       text: 'Cancelar',
+    //       style: 'cancel',
+    //     },
+    //     {
+    //       text: 'Remover',
+    //       style: 'destructive',
+    //       onPress: () => {
+    //         setLembretes((lembretesAtual) => {
+    //           return lembretesAtual.filter((l) => l.id !== lembrete.id)
+    //         })  
+    //       }
+    //     }
+    //   ]
+    // )
+    setLembretes(lembretesAtual => lembretesAtual.filter(l => l.id !== lembrete.id))
+  }
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -38,9 +69,36 @@ export default function App() {
           style={styles.button}>
           <Text style={styles.buttonText}>Salvar</Text>
         </Pressable>
-        {
-          lembretes.map(l => <Text>{l.texto}</Text>)
-        }
+        <FlatList 
+          style={styles.list}
+          keyExtractor={(l) => l.id}
+          data={lembretes}
+          ItemSeparatorComponent={() => <View style={{marginVertical: 4}}></View>}
+          // ListFooterComponent={() => <FontAwesome name='github' size={40}/>}
+          renderItem={(l) => {
+            return (
+              <View style={styles.listItem}>
+                <Text
+                  style={styles.listItemText}>
+                  {l.item.texto}
+                </Text>
+                <View style={styles.listItemButtons}>
+                  <Pressable
+                    onPress={() => remover(l.item)}>
+                    <FontAwesome name="trash-o" size={24} color={'red'}/>
+                  </Pressable>
+                  <Pressable>
+                    <FontAwesome name="pencil" size={24} color={'blue'}/>
+                  </Pressable>
+                </View>
+              </View>
+            )
+          }}/>
+          <View style={{borderWidth: 1, borderColor: '#EEE', borderRadius: 4, width: '80%', alignItems: 'center', marginTop: 12, paddingVertical: 8}}>
+            <Pressable onPress={() => Linking.openURL('https://github.com/professorbossini')}>
+                <FontAwesome name='github' size={40}/>
+            </Pressable>
+          </View>
     </View>
   );
 }
@@ -50,7 +108,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    paddingVertical: 18
   },
   input: {
     width: '80%',
@@ -70,5 +129,58 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     textAlign: 'center'
+  },
+  list: {
+    marginTop: 12, 
+    borderWidth: 1, 
+    borderColor: '#DDD', 
+    borderRadius: 8, 
+    width: '80%',
+    padding: 8
+  },
+  listItem: {
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBlockColor: "#EEE",
+    backgroundColor: '#F0F0F0',
+    textAlign: 'center',
+    borderRadius: 4,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  listItemText: {
+    textAlign: 'center',
+    width: '70%'
+  },
+  listItemButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '30%'
   }
 });
+
+// [
+//   {
+//     item: {
+//       id: 1,
+//       texto: 'abc'
+//     }
+//   },
+//   {
+//     item: {
+//       id: 2,
+//       texto: 'abcd'
+//     }
+//   }
+// ]
+
+// [
+//   {
+//     id: 1,
+//     texto: 'abc'
+//   },
+//   {
+//     id: 2,
+//     texto: 'abcd'
+//   }
+// ]
